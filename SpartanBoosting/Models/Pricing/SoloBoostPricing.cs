a469917b3c067ac7bd37aa0dc88d9ee7354c54ed
Robert Wardell <Robert.Wardell@sgsco.com>
2020-07-30 01:02:04 +0100

    Load data from xcel

---
+++ SpartanBoosting/Models/Pricing/SoloBoostPricing.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpartanBoosting.Models.Pricing
+{
+	public class SoloBoostPricing
+	{
+		public string CurrentDivision { get; set; }
+		public string CurrentLP { get; set; }
+		public string RequiredDivision { get; set; }
+		public string GameboostPrice { get; set; }
+		public string OurPrice { get; set; }
+	}
+}
