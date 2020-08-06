6d34ba38305bb851692d2dd10005323124f0b590
Robert Wardell <Robert.Wardell@sgsco.com>
2020-08-06 17:02:54 +0100

    changes

--- SpartanBoosting/Controllers/AdhocController.cs
+++ SpartanBoosting/Controllers/AdhocController.cs
@@ -3,11 +3,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using SpartanBoosting.Utils;
 
 namespace SpartanBoosting.Controllers
 {
     public class AdhocController : Controller
     {
+        private readonly IOptions<SmtpSettings> _smtpSettings;
+
+        public AdhocController(IOptions<SmtpSettings> smtpSettings)
+        {
+            _smtpSettings = smtpSettings;
+
+        }
         public IActionResult PriceBeatGuarantee()
         {
             return View();
@@ -23,6 +32,8 @@ namespace SpartanBoosting.Controllers
 
         public IActionResult ContactUs()
         {
+            EmailSender test = new EmailSender(_smtpSettings);
+            test.SendEmailAsync("", "", "");
             return View();
         }
     }
--- SpartanBoosting/Utils/EmailSender.cs
+++ SpartanBoosting/Utils/EmailSender.cs
@@ -7,8 +7,12 @@ using System.Threading.Tasks;
 
 namespace SpartanBoosting.Utils
 {
-    public class EmailSender 
-    {
+	public interface IEmailSender
+	{
+		Task SendEmailAsync(string email, string subject, string message);
+	}
+	public class EmailSender : IEmailSender
+	{
         private readonly SmtpSettings _smtpSettings;
 
         public EmailSender(IOptions<SmtpSettings> smtpSettings)
@@ -18,7 +22,27 @@ namespace SpartanBoosting.Utils
         }
         public async Task SendEmailAsync(string email, string subject, string htmlMessage)
         {
-            //https://kenhaggerty.com/articles/article/aspnet-core-22-smtp-emailsender-implementation
+            try
+            {
+                MailMessage message = new MailMessage();
+                message.Subject = subject;
+                message.Body = htmlMessage;
+                message.IsBodyHtml = true;
+                message.To.Add("bobbywardell@hotmail.com");
+
+                string host = _smtpSettings.Server;
+                int port = _smtpSettings.Port;
+                string fromAddress = _smtpSettings.FromAddress;
+
+                message.From = new MailAddress(fromAddress);
+                using (var smtpClient = new SmtpClient(host, port))
+                {
+                    smtpClient.Credentials = new System.Net.NetworkCredential("SpartanBoosting@robertdomain.com", "Tel829988@");
+                    await smtpClient.SendMailAsync(message);
+                }
+            } catch (Exception e ) 
+            { 
+            }
         }
     }
 }
