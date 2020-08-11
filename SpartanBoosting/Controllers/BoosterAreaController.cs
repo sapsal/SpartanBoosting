f381a582691b2ab80f20fd8f286de5d88524b478
Robert Wardell <Robert.Wardell@sgsco.com>
2020-08-11 22:28:18 +0100

    Add Keen themes

---
+++ SpartanBoosting/Controllers/BoosterAreaController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SpartanBoosting.Controllers
+{
+    public class BoosterAreaController : Controller
+    {
+        public IActionResult Dashboard()
+        {
+            return View();
+        }
+    }
+}
