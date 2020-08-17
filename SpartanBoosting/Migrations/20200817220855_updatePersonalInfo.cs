b5997d4db54aa243bba9f0eee2c432927843f12a
Robert Wardell <Robert.Wardell@sgsco.com>
2020-08-17 23:15:37 +0100

    update ef

---
+++ SpartanBoosting/Migrations/20200817220855_updatePersonalInfo.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace SpartanBoosting.Migrations
+{
+    public partial class updatePersonalInfo : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "JobAvailable",
+                table: "PurchaseForm",
+                nullable: false,
+                defaultValue: false);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PayPalApproval",
+                table: "PurchaseForm",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "PayPalCapture",
+                table: "PurchaseForm",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "JobAvailable",
+                table: "PurchaseForm");
+
+            migrationBuilder.DropColumn(
+                name: "PayPalApproval",
+                table: "PurchaseForm");
+
+            migrationBuilder.DropColumn(
+                name: "PayPalCapture",
+                table: "PurchaseForm");
+        }
+    }
+}
