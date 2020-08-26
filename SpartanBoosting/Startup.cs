566fa4ec845da74928275d1929da5f85d0b3dc12
Robert Wardell <Robert.Wardell@sgsco.com>
2020-08-26 16:57:05 +0100

    add changes to inprogress and completed jobs

--- SpartanBoosting/Startup.cs
+++ SpartanBoosting/Startup.cs
@@ -64,7 +64,7 @@ namespace SpartanBoosting
 			{
 				// Cookie settings
 				options.Cookie.HttpOnly = true;
-				options.ExpireTimeSpan = TimeSpan.FromMinutes(5);
+				options.ExpireTimeSpan = TimeSpan.FromMinutes(60);
 
 				//options.LoginPath = "/Identity/Account/Login";
 				//options.AccessDeniedPath = "/Identity/Account/AccessDenied";
