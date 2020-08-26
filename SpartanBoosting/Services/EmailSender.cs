bfab115c33c00c3cab8a72693edb7715e9d3fa41
Robert Wardell <Robert.Wardell@sgsco.com>
2020-08-26 23:05:35 +0100

    fixes

--- SpartanBoosting/Services/EmailSender.cs
+++ SpartanBoosting/Services/EmailSender.cs
@@ -1,6 +1,9 @@
-﻿using System;
+﻿using Microsoft.Extensions.Options;
+using SpartanBoosting.Utils;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace SpartanBoosting.Services
@@ -9,9 +12,40 @@ namespace SpartanBoosting.Services
     // For more details see https://go.microsoft.com/fwlink/?LinkID=532713
     public class EmailSender : IEmailSender
     {
-        public Task SendEmailAsync(string email, string subject, string message)
+        private readonly SmtpSettings _smtpSettings;
+
+        public EmailSender(IOptions<SmtpSettings> smtpSettings)
         {
-            return Task.CompletedTask;
+            _smtpSettings = smtpSettings.Value;
+
+        }
+        public async Task SendEmailAsync(string email, string subject, string Message)
+        {
+            try
+            {
+                MailMessage message = new MailMessage();
+                message.Subject = subject;
+                message.Body = Message;
+                message.IsBodyHtml = true;
+                message.To.Add(email);
+
+                string host = _smtpSettings.Server;
+                int port = _smtpSettings.Port;
+                string fromAddress = _smtpSettings.FromAddress;
+
+                message.From = new MailAddress(fromAddress);
+                using (var smtpClient = new SmtpClient(host, port))
+                {
+                    smtpClient.EnableSsl = true;
+                    smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+                    smtpClient.UseDefaultCredentials = false;
+                    smtpClient.Credentials = new System.Net.NetworkCredential("info@spartanboosting.com", "P4yp4lP4ssw0rd-?!");
+                    await smtpClient.SendMailAsync(message);
+                }
+            }
+            catch (Exception e)
+            {
+            }
         }
     }
 }
